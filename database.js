787ad1a6043832a71bd2e463c988dff684dde2cf
Marijn Heuts <marijn.heuts@maxserv.com>
2025-04-08 16:46:15 +0200

    added docker compose

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
 const {MongoClient} = require('mongodb');
+require('dotenv');
 const uri = process.env.MONGO_URL;
+console.log("Mongo URI: ", uri);
 const client = new MongoClient(uri);
 const db = client.db(process.env.DB_NAME || "defaultDB");  
 
